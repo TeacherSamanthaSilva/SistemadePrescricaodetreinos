a93694a1d8afe6be4d4d188f5df85cc72b7158f7
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2026-03-31 18:02:11 -0300

    Create exercises_db.py

---
+++ exercises_db.py
@@ -0,0 +1,21 @@
+exercises = [
+    # PUSH
+    {"name": "Flexão de braço", "level": "iniciante", "group": "push", "equipment": "nenhum"},
+    {"name": "Flexão inclinada", "level": "iniciante", "group": "push", "equipment": "nenhum"},
+    {"name": "Flexão declinada", "level": "intermediario", "group": "push", "equipment": "nenhum"},
+    {"name": "Pseudo planche push-up", "level": "avancado", "group": "push", "equipment": "nenhum"},
+
+    # PULL
+    {"name": "Remada australiana", "level": "iniciante", "group": "pull", "equipment": "barra"},
+    {"name": "Barra fixa", "level": "intermediario", "group": "pull", "equipment": "barra"},
+    {"name": "Archer pull-up", "level": "avancado", "group": "pull", "equipment": "barra"},
+
+    # LEGS
+    {"name": "Agachamento", "level": "iniciante", "group": "legs", "equipment": "nenhum"},
+    {"name": "Afundo", "level": "iniciante", "group": "legs", "equipment": "nenhum"},
+    {"name": "Pistol squat", "level": "avancado", "group": "legs", "equipment": "nenhum"},
+
+    # CORE
+    {"name": "Prancha", "level": "iniciante", "group": "core", "equipment": "nenhum"},
+    {"name": "Elevação de pernas", "level": "intermediario", "group": "core", "equipment": "barra"},
+]
